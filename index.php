e94f7ba1a0649f7e2529deebf1ff035ec7b8a122
ml lombry <webmaster@resinence.fr>
2018-05-22 11:49:11 +0200

    my first commit for a simple test of git

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset='utf-8'>
+	
+	</head>
+	<body>
+		<h1 id='h'>test</h1>
+		<form method ='post' action='' id='myForm'>
+			<label for ='choise'>Food</label>
+			<select name='choise' id='choise' onchange="check()">
+				<option value='pain'>Pain</option>
+				<option value='pizza'>pizza</option>
+				<option value='salade'>salade</option>
+				<option value='merguez'>merguez</option>
+				<option value='burger'>burger</option>
+			</select>
+		
+		</form>
+	</body>
+		<script>
+	
+		/*
+		let req = new XMLHttpRequest();
+		req.onload = function(){
+		  document.querySelector('h1').innerHTML=this.response
+		}
+		
+		req.open('get','https://developer.mozilla.org/fr/docs/Web/API/XMLHttpRequest/Utiliser_XMLHttpRequest',true)
+		
+		req.send()
+		
+		new Promise((resolve, reject) =>{
+			console.log('initial')
+			
+			resolve();
+			
+		})
+		.then(()=>{
+			throw new Error('Something failed')
+			console.log('do this')
+		
+		})
+		.catch(()=>{
+			console.log('do that')
+		}).then(()=>{
+			console.log('do this whatever happened before')
+		})
+		*/
+		
+		// commander es une fonction qui prend un argument() qui retourne une promise 
+		
+			
+		
+	
+		let food = ['burger','pizza']
+		const commander = arg =>{
+			return new Promise((resolve,reject)=>{
+				food.includes(arg)?resolve('votre produit est disponible') : reject('Une erreur est survenue le produit est en rupture')	
+			})
+		}
+		
+		commander('pizza').then(
+			resultat => document.querySelector('h1').innerHTML = resultat,
+			error => document.querySelector('h1').innerHTML = error
+		)
+		
+		const check =()=>{
+			let c = document.getElementById('choise').value
+			commander(c).then(
+			resultat => document.querySelector('h1').innerHTML = resultat,
+			error => document.querySelector('h1').innerHTML = error
+		)
+		}
+		
+		</script>
+</html>
